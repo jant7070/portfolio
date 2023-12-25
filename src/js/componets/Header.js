941e1b42eb4c1afec009b92e8367b857e32494fc
jant7070 <josemorillo702@gmail.com>
2023-12-25 16:04:23 -0400

    Home page finished

--- src/js/componets/Header.js
+++ src/js/componets/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/header.css";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Header = () => {
 
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg nav-bar">
+            <nav className="navbar navbar-expand-lg nav-bar sticky-top">
                 <div className="container-fluid">
                     <Link className="link-tag" to="/">{`< Jose Morillo />`}</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span>A</span>
+                    <button className="navbar-toggler bg-transparent border border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span><FontAwesomeIcon icon={faBars} /></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
                         <ul className="navbar-nav">
--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "../../styles/home.css"
-
+import ReactTyped from "react-typed";
+import { Link } from "react-router-dom";
 
 
 export const Home = () => {
     return (
-        <div>
-            <h1>Soy el Home</h1>
+        <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
+            <div className="presentation mx-4 container">
+                <div className="row justify-content-md-center">
+                    <div className="col-md-auto text-center">
+                        <h1>
+                            <ReactTyped
+                                strings={["Hello!", "Hola!", "こんにちは", "Bonjour", "你好", "привет"]}
+                                typeSpeed={200}
+                                backSpeed={200}
+                                loop />
+
+                        </h1>
+                    </div>
+                    <div className="col-md-auto text-center">
+                        <h1>I'm Jose Morillo</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="description text-center mx-4">
+                <p>
+                    A curious, passionate, and autodidact fullstack developer from Venezuela.<br />
+                    With a keen eye for building web applications with modern technologies.
+                </p>
+                <Link className="button" to="/proyects">Proyects</Link>
+            </div>
         </div>
     )
 }
